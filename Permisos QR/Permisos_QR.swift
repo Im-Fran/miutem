2b5aab35571592b570eb5f2a615b6d38da6cdc41
Francisco Solis <imfran@duck.com>
2023-09-14 21:37:50 -0300

    feat(splash-y-widget): agregado splash
    * Agregado Launch Screen (splash) nativo
    * Trabajando en Widget para mostrar QR en la pantalla bloqueada.

---
+++ Permisos QR/Permisos_QR.swift
@@ -0,0 +1,17 @@
+//
+//  Permisos_QRBundle.swift
+//  Permisos QR
+//
+//  Created by Francisco Solis Maturana on 14-09-23.
+//
+/*
+import WidgetKit
+import SwiftUI
+
+@main
+struct PermisosQR: WidgetBundle {
+    var body: some Widget {
+        
+    }
+}
+*/
